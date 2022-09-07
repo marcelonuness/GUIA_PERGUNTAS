5121b044c0dd56c454a8b53a0d84ee534c978710
Marcelo Nunes <marcelohenrique.nunes@gmail.com>
2022-09-07 16:51:02 -0300

    v2

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+
+//Estou dizendo ao ExpressJS usar o EJS como View engine
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
+
+
+app.listen(8080, () => {
+    console.log("App rodando!")
+})
